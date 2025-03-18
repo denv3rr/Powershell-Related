473e1c3ba9d834c8a717390d44433fd0b9a2b9e4
denv3rr <denver.clark@g.austincc.edu>
2025-03-17 20:36:55 -0500

    add test files for pwsh animations, add Neofetch tests for a native Windows version, add CRLF/LF warnings command info

---
+++ git-crlf-warnings-dismiss.sh
@@ -0,0 +1,22 @@
+# CRLF/LF related configs for git
+# Warning will say: warning: CRLF will be replaced by LF in <file_name>
+
+# Warnig dismissal options:
+# ---------------------------------------------------
+
+# Set Git to Use LF for All Commits (Cross-Platform Development)
+git config --global core.autocrlf input
+
+# Disable the Warning (Only Suppresses the Message)
+git config --global core.safecrlf false
+
+# Force Git to Always Use LF (for Linux/macOS Development)
+git config --global core.eol lf
+git config --global core.autocrlf false
+
+# Convert All Files to LF Immediately
+git add --renormalize .
+git commit -m "Normalize line endings"
+
+# To Check Your Current Line Ending Configuration Run:
+git config --global --list | grep crlf
